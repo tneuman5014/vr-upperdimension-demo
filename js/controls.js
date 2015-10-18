4ce8193929913daad35b21a2ef9b6ee8a2aca72f
tneuman5014 <tneuman@UW.edu>
2015-10-17 23:18:53 -0700

    Initial implementation of VR control/effects

---
+++ js/controls.js
@@ -0,0 +1,20 @@
+var KEY = {W:0, A:1, S:2, D:3};
+
+function handle(key, camera) {
+	switch (key) {
+		case 'w':
+			move(camera, {x:0, y:0.12, z:0});
+		case 'a':
+			move(camera, {x:0, y:0, z:0.12});
+		case 's':
+			move(camera, {x:0, y:0, z:0.12});
+		case 'd':
+			move(camera, {x:0.12, y:0, z:0});
+	}
+}
+
+function move(camera, vector) {
+	camera.position.x += vector.x;
+	camera.position.y += vector.y;
+	camera.position.z += vector.z;
+}
---
+++ js/makers.js
@@ -0,0 +1,20 @@
+var SHORT_SIDE_LENGTH = 2;
+var LONG_SIDE_LENGTH  = 4;
+
+var MATERIAL = THREE.MeshBasicMaterial({color:0xff0000});
+
+function generateBox(x, y, z, height) {
+	var geometry = new THREE.BoxGeometry(SHORT_SIDE_LENGTH, LONG_SIDE_LENGTH, height);
+	var result = new THREE.Mesh(geometry, MATERIAL);
+	result.position.x = x;
+	result.position.y = y;
+	result.position.z = z;
+	return result;
+}
+
+function generateLine(startvector, endvector) {
+	var geometry = new THREE.Geometry();
+	geometry.vertices.push(startvector, endvector);
+	var mat = new THREE.LineBasicMaterial({color:0x000ff});
+	return new THREE.Line(geometry, mat);
+}
