ea7e9aa2b24dc723cdab3f17eae0ce84c2cc56fb
tneuman5014 <tneuman@UW.edu>
2015-10-18 01:15:06 -0700

    Changed controls
    Re-added original WASD controls, changed full screen button to 'H'

--- js/controls.js
+++ js/controls.js
@@ -1,19 +1,25 @@
 var KEY = {W:0, A:1, S:2, D:3};
 
 function handle(key, camera, vrEffect) {
-	/*switch (key) {
+	switch (key) {
 		case 'w':
 			move(camera, {x:0, y:0.12, z:0});
+			break;
 		case 'a':
 			move(camera, {x:0, y:0, z:0.12});
+			break;
 		case 's':
 			move(camera, {x:0, y:0, z:0.12});
+			break;
 		case 'd':
 			move(camera, {x:0.12, y:0, z:0});
-	}*/
-	vrEffect.setFullScreen(true);
-	console.log("Enter full screen");
-	vrEffect.setSize(window.innerWidth, window.innerHeight);
+			break;
+		case 'h':
+			vrEffect.setFullScreen(true);
+			vrEffect.setSize(window.innerWidth, window.innerHeight);
+			break;
+	}
+
 }
 
 function move(camera, vector) {
