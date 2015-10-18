58be6c9366d257d67abbb290ce2aa06b45e21e46
tneuman5014 <tneuman@UW.edu>
2015-10-18 00:38:55 -0700

    Fixed minor bug in makers.js

--- js/makers.js
+++ js/makers.js
@@ -1,7 +1,7 @@
 var SHORT_SIDE_LENGTH = 2;
 var LONG_SIDE_LENGTH  = 4;
 
-var MATERIAL = THREE.MeshBasicMaterial({color:0xff0000});
+var MATERIAL = new THREE.MeshBasicMaterial({color:0xff0000});
 
 function generateBox(x, y, z, height) {
 	var geometry = new THREE.BoxGeometry(SHORT_SIDE_LENGTH, LONG_SIDE_LENGTH, height);
